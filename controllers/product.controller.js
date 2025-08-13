ef9ed8d8557732a50466778f01b222fc019969ff
Asfandyar <asfandm17@gmail.com>
2025-08-13 22:52:29 +0500

    feat: product update and delete api

--- controllers/product.controller.js
+++ controllers/product.controller.js
@@ -51,4 +51,49 @@ const getProducts = asyncHandler(async (req, res) => {
   });
 });
 
-export { createProduct, getProducts };
+const getProduct = asyncHandler(async (req, res) => {
+  const product = await Product.findById(req.params.id).populate(
+    "createdBy",
+    "name email role avatar"
+  );
+
+  if (!product) {
+    return res.status(404).json({
+      success: false,
+      message: "Products not found!",
+    });
+  }
+  res.status(200).json({ success: true, product });
+});
+
+const updateProduct = asyncHandler(async (req, res) => {
+  const product = await Product.findByIdAndUpdate(req.params.id, req.body, {
+    new: true,
+  });
+
+  if (!product) {
+    return res.status(400).json({
+      success: false,
+      message: "something went wrong while updating product. Try Again!",
+    });
+  }
+
+  res.status(200).json({ success: true, message: "product updated!", product });
+});
+const deleteProduct = asyncHandler(async (req, res) => {
+  const product = await Product.findByIdAndUpdate(
+    req.params.id,
+    { isDeleted: true },
+    { new: true }
+  );
+
+  if (!product) {
+    return res
+      .status(404)
+      .json({ success: false, message: "Product not found!" });
+  }
+
+  res.status(200).json({ success: true, message: "Product soft deleted!" });
+});
+
+export { createProduct, getProducts, getProduct, updateProduct, deleteProduct };
--- routes/product.routes.js
+++ routes/product.routes.js
@@ -1,12 +1,18 @@
 import express from "express";
 import {
   createProduct,
+  deleteProduct,
+  getProduct,
   getProducts,
+  updateProduct,
 } from "../controllers/product.controller.js";
 import { admin, auth } from "../middlewares/auth.js";
 const router = express.Router();
 
 router.post("/product", auth, admin, createProduct);
 router.get("/products", getProducts);
+router.get("/products/:id", getProduct);
+router.patch("/products/:id", updateProduct);
+router.delete("/products/:id", deleteProduct);
 
 export default router;
